75c73d002596acaa194594d05caf4a70da30bd01
Markus Kilås <markus@markuspage.com>
2015-07-31 17:05:58 +0200

    Adding packages for future implementations

---
+++ fewtimesignature/src/main/java/se/kilas/markus/qryptostuff/fewtimesignature/horst/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Future HORS with tree (HORST) - an improvement of the HORS few-time
+ * signature scheme - implementation.
+ * 
+ * TODO
+ */
+package se.kilas.markus.qryptostuff.fewtimesignature.horst;
---
+++ statelesshashsignature/src/main/java/se/kilas/markus/qryptostuff/statelesshashsignature/sphincs/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Future SPHINCS - practical stateless hash-based signatures - implementation.
+ * 
+ * TODO
+ */
+package se.kilas.markus.qryptostuff.statelesshashsignature.sphincs;
