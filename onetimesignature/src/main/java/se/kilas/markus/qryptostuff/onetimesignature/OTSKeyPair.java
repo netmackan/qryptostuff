ac79669e0a34ddf11b63c09c1e1d1a150477eeae
Markus Kilås <markus@markuspage.com>
2015-07-31 14:27:04 +0200

    Adding author to JavaDoc/headers

--- onetimesignature/src/main/java/se/kilas/markus/qryptostuff/onetimesignature/OTSKeyPair.java
+++ onetimesignature/src/main/java/se/kilas/markus/qryptostuff/onetimesignature/OTSKeyPair.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 user
+ * Copyright (C) 2015 Markus Kilås
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- onetimesignature/src/main/java/se/kilas/markus/qryptostuff/onetimesignature/OTSKeyPairGenerator.java
+++ onetimesignature/src/main/java/se/kilas/markus/qryptostuff/onetimesignature/OTSKeyPairGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 user
+ * Copyright (C) 2015 Markus Kilås
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@ package se.kilas.markus.qryptostuff.onetimesignature;
 
 /**
  *
- * @author user
+ * @author Markus Kilås
  */
 public interface OTSKeyPairGenerator {
     OTSKeyPair generate();
